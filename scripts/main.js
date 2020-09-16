2a52c0acc2995cdfaba244c43e567c8505c0f690
Daniel <daniel.moderiano@outlook.com>
2020-09-16 16:19:20 +0930

    Added function to return div id on click in board divs

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@ const gameBoard = (function() {
     const board = ["X", "X", "O",
                    "X", "O", "O",            
                    "O", "X", "X"];
+    
+  
+
+    
 
     return { board };           
 })();
@@ -9,7 +13,12 @@ const gameBoard = (function() {
 const player = function(name, symbol) {
     const _playerNameCapitalise = (name) => name.toUpperCase();
     const playerNameDisplay = () => console.log(_playerNameCapitalise(name));
+    
+    
+    
+
     return { name, symbol, playerNameDisplay };
+  
 }   
 
 const game = (function() {
@@ -21,12 +30,21 @@ const game = (function() {
 const boardSquares = document.querySelectorAll(".board__square");
 const updateBtn = document.querySelector(".button--update");
 
+
+boardSquares.forEach(function(square) {
+    square.addEventListener('click', function(e) {
+  
+        console.log(e.target.getAttribute("data-id"));
+    });
+});
+
+
 updateBtn.addEventListener('click', () => render(gameBoard.board, boardSquares));
 
 
 const render = function(array, targetDivs) {
     for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
+        console.log(targetDivs[i].getAttribute("data-id"));
         targetDivs[i].textContent = array[i];
     }
     return ("done");
