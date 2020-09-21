90e711e205f40598006cc632b63f11fe0f2af973
Daniel <daniel.moderiano@outlook.com>
2020-09-21 19:11:44 +0930

    Fixed error in checkWin (type only)

--- scripts/main.js
+++ scripts/main.js
@@ -36,7 +36,7 @@ const checkWin = function() {
         (gameBoard.board[0] === gameBoard.board[3] && gameBoard.board[0] === gameBoard.board[6] && 
         gameBoard.board[0] != "") || 
         (gameBoard.board[1] === gameBoard.board[4] && gameBoard.board[1] === gameBoard.board[7] && 
-        gameBoard.board[6] != "") || 
+        gameBoard.board[7] != "") || 
         (gameBoard.board[2] === gameBoard.board[5] && gameBoard.board[2] === gameBoard.board[8] && 
         gameBoard.board[2] != "") ||
     
@@ -64,9 +64,10 @@ boardSquares.forEach(function(square) {
         gameBoard.board[e.target.getAttribute("data-id")] = "X";
         render(gameBoard.board, boardSquares);
         if (checkWin()) {
-            alert("You win!");
+       
+            console.log("You win!");
             gameBoard.clearBoard();
-            render(gameBoard.board, boardSquares);
+            // render(gameBoard.board, boardSquares);
         }
         
     });
